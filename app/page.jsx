f29929b55fdd415de009bc0261ab211f4c0d46a6
ParadigmaRama <ramirogimenezcontacto@gmail.com>
2023-08-16 12:04:32 -0300

    First commit page

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage(){
+	return(
+		<section>
+			<header>Header</header>
+			<article>Los posts</article>
+			<aside>Al lado</aside>
+			<footer>el footer</footer>
+		</section>
+	)
+}
---
+++ app/welcome/page.jsx
@@ -0,0 +1,7 @@
+export default function WelcomePage(){
+	return(
+		<article>
+			<h1>esta es la pagina de bienvenida</h1>
+		</article>
+	)
+}
